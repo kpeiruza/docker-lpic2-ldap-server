d72cd1d3b25f6dd8069178358aee0e40b616e840
Kenneth Peiruza <kenneth@floss.cat>
2016-06-28 18:15:44 +0200

    Slapd.sh didn't sent!

--- slapd.sh
+++ slapd.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -e
-/usr/sbin/slapd -h "ldap:/// ldapi:///" -g openldap -u openldap -F /etc/ldap/slapd.d -d 3
+#	Launch Slapd and keep it "interactive"
+/usr/sbin/slapd -h "ldap:/// ldapi:///" -g openldap -u openldap -F /etc/ldap/slapd.d
+#-d 3
+echo "Adding LDIF"
+sed "s/REPLACETLD/dc=floss,dc=cat/" /root/tree.ldif | ldapadd -x -D "cn=admin,dc=floss,dc=cat" -wpassword -H ldap://localhost 
+#	Launch SSH Server
+/usr/sbin/sshd -D
